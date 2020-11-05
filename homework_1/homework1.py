db17e3f80ff7d1c760782c4be7c2a4f1cee75588
Olga Pascal <olga.pascal@gmail.com>
2020-11-05 23:13:24 +0200

    homework #1

---
+++ homework_1/homework1.py
@@ -0,0 +1,61 @@
+name = "Olga"
+age = "39"
+print("My name is ", name, "My age is ", age)
+a = int(input(f"Enter the first number for"))
+print(type(a))
+
+print(a + 10)
+print("The number is OK")
+print(f" you entered {a}")
+b = input("Enter some string")
+print(f"you entered {b}")
+
+print(f"ljdfglkdjfglkdfjg dglkdjfglkdjfg dflgkdj {b} ")
+print(" some string information not dynamic ")
+
+# task 2
+seconds = int(input("Enter seconds"))
+hour = seconds // 3600
+minute = seconds // 60
+print(f"Time is {hour:02}:{minute:02}:{seconds:02}")
+
+# task 3
+number = int(input("Enter number: "))
+a = 2 * "{}".format(number)
+b = 3 * "{}".format(number)
+sum = number + int(a) + int(b)
+print("{} {} {} {}".format(number, a, b, sum))
+
+# task 4
+number = str(input("Enter min 4-digit number : "))
+sp = [n for n in number]
+print(max(sp))
+
+# task5
+profitability = 0
+profit = int(input("Enter profit value"))
+expenses = int(input("Enter expenses"))
+if profit > expenses:
+    profitability = profit - expenses
+    pty = profitability / profit
+    print(f"Company is profitable, the profitability is {pty}")
+else:
+    print("Company is not profitable")
+    members = int(input("Company members number: "))
+    permember = profitability / members
+    print(f"Member salary would be: {permember}")
+
+
+# task6
+
+a = int(input("Enter result for day 1: "))
+challenge = int(input("Enter challenge in km: "))
+day = 1
+each_next_day = a / 100 * 10
+next_day_result = each_next_day
+print(f"each next day {each_next_day}")
+while next_day_result < challenge:
+    next_day_result += each_next_day * day
+    day += 1
+    print(f"{next_day_result} day {day}")
+print(f"Result will be achieved on {day} day")
